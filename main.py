54b64df45db3d558bbb34780f260c033440d89c3
Riho0511 <riho.pinehill@gmail.com>
2023-03-10 20:28:35 +0900

    discord.py導入

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from generateCharacterImage import generater
+# from generateCharacterImage import generater
 
-if __name__ == "__main__":
-    generater.generation(generater.read_json('data.json'))
+# if __name__ == "__main__":
+#     generater.generation(generater.read_json('data.json'))
+
+import discord
+import config
+
+# 必要インスタンス生成
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+
+# スラッシュコマンド作成
+@tree.command(name="test", description="テストコマンド")
+async def test_command(interaction: discord.Interaction):
+    await interaction.response.send_message("てすと！")
+
+
+# 起動時のイベント
+@client.event
+async def on_ready():
+    print('起動しました')
+    await tree.sync()
+
+
+client.run(config.BOT_TOKEN)
